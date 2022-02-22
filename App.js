8cf865676d41f45b4d41af6af348a66cc74811eb
Riya-Soni-Github01 <riya.soni@amplework.com>
2022-02-22 10:42:32 +0530

    initial

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import PushController from './PushController';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, FlatList } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+      <PushController />
+
+    </View>
+  );
+};
+export default App;
